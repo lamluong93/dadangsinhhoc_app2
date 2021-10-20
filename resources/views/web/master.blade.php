4c7cd1245143af3348bfddd46c7727c6a9fb7ad9
duyanh84 <duyanhse04697@gmail.com>
2021-10-20 10:00:47 +0700

    sửa thanh search ở trang chủ

--- resources/views/web/master.blade.php
+++ resources/views/web/master.blade.php
@@ -82,7 +82,7 @@
 								<a class="dropdown-item" href="hosting.html">NBDS API</a>
 							</div>
 						</li>
-						<li class="nav-item"><a class="nav-link" href="contact.html">Liên hệ</a></li>
+						<!-- <li class="nav-item"><a class="nav-link" href="contact.html">Liên hệ</a></li> -->
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
                         @if (Route::has('login'))
